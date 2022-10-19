a260820e96ee2340a52bfcdc9e726b60e10c746c
DavideSias <davidesias77@gmail.com>
2022-10-19 15:26:56 +0200

    Add js script

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ Chiedere cognome all'utente
 Chiedere colore preferito all'utente
 Stampare nome + cognome + colore preferito + 22
 */
+
+let nome = prompt('Inserire nome');
+let cognome = prompt('Inserire cognome');
+let colore = prompt('Inserire colore preferito');
+let passGen = nome + cognome + colore + '22';
+document.getElementById('password').innerHTML = passGen;
